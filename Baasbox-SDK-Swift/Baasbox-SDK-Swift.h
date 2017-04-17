bd37933cce88a99bed932ab5a3bb97e6e57443da
Lyubomir Marinov <lyubomir.marinov@kaufland.de>
2017-04-17 18:22:00 +0300

    Initial Commit

---
+++ Baasbox-SDK-Swift/Baasbox-SDK-Swift.h
@@ -0,0 +1,19 @@
+//
+//  Baasbox-SDK-Swift.h
+//  Baasbox-SDK-Swift
+//
+//  Created by Lyubomir Marinov on 4/17/17.
+//  Copyright © 2017 Skylark Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Baasbox-SDK-Swift.
+FOUNDATION_EXPORT double Baasbox_SDK_SwiftVersionNumber;
+
+//! Project version string for Baasbox-SDK-Swift.
+FOUNDATION_EXPORT const unsigned char Baasbox_SDK_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Baasbox_SDK_Swift/PublicHeader.h>
+
+
